d67b2c967466d522e557f55d39676bda0face82d
TimoBroeskamp <ti.broeskamp@gmail.com>
2026-03-21 16:58:27 +0100

    added scuffed shooting

---
+++ roguelike/GlobalData.gd
@@ -0,0 +1,4 @@
+extends Node
+
+@onready var World = $"../World"
+@onready var Player = $"../World/Player"
---
+++ roguelike/Window.gd
@@ -0,0 +1,8 @@
+extends Window
+
+func _ready():
+	mode = Window.MODE_MAXIMIZED
+	
+	borderless = true
+	
+	visible = true
