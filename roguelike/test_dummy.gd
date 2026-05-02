656d881e4a0bf55bf847338491b1a17771034c27
TimoBroeskamp <ti.broeskamp@gmail.com>
2026-05-02 18:02:51 +0200

    finished health system with healthbars

---
+++ roguelike/test_dummy.gd
@@ -0,0 +1,19 @@
+extends CharacterBody2D
+
+@onready var health_component: Health = $Health
+@onready var health_bar = $Healthbar
+
+func _ready():
+	health_bar.init_health(health_component.max_health)
+	
+	health_component.health_changed.connect(func(_diff):
+		health_bar.health = health_component.health
+		)
+	
+	health_component.max_health_changed.connect(func(_diff):
+		health_bar.max_value = health_component.max_health
+		health_bar.damage_bar.max_value = health_component.max_health
+	)
+	
+func _on_health_health_depleted() -> void:
+	queue_free()
