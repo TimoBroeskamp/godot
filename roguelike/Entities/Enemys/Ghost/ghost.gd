326ff849e68eeee8f167c378317994933566cd9d
Marc Timo Bröskamp <broeskamp@kitconcept.com>
2026-09-24 12:07:44 +0200

    added ghost enemy and fixed main map

---
+++ roguelike/Entities/Enemys/Ghost/ghost.gd
@@ -0,0 +1,44 @@
+extends Enemy
+class_name Ghost
+
+@onready var player = get_tree().get_first_node_in_group("player")
+@onready var navigation = $Navigation
+@onready var hitbox: HitBox = $HitBox
+@onready var stats: Stats = $Stats
+@onready var health_component: Stats = $Stats
+@onready var health_bar = $Healthbar
+
+func _ready():
+	health_bar.init_health(health_component.max_health)
+	
+	health_component.health_changed.connect(func(_diff):
+		health_bar.health = health_component.health
+		)
+	
+	health_component.max_health_changed.connect(func(_diff):
+		health_bar.max_value = health_component.max_health
+		health_bar.damage_bar.max_value = health_component.max_health
+	)
+	
+	hitbox.damage = stats.damage
+	stats.health_depleted.connect(_on_death)
+
+func respawn() -> void:
+	pass
+
+func _on_death() -> void:
+	died.emit(self)
+
+
+func _physics_process(delta: float) -> void:
+	move_to_player()
+	move_and_slide()
+# Movement
+
+func move_to_player() -> void:
+	velocity = navigation.move_direction * stats.movement_speed
+
+func reset_state() -> void:
+	health_component.health = health_component.max_health
+	# reset anim state, velocity, current nav target, any timers here too
+	velocity = Vector2.ZERO
